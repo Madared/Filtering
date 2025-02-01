76f08cec8dbf20e26cba9c6a2bde8842c376aaa5
Afonso <afonsomtsm@gmail.com>
2025-02-01 15:51:42 +0000

    Updated version

--- Filtering/Filterable/Async/IAsyncFilterableExecutable.cs
+++ Filtering/Filterable/Async/IAsyncFilterableExecutable.cs
@@ -4,12 +4,43 @@ using ResultAndOption.Results;
 
 namespace Filtering.Filterable.Async;
 
-public interface IAsyncFilterableExecutable<T> : IFilterable<T, IFilterableExecutable<T>> where T : notnull
+/// <summary>
+/// An async executable filterable 
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IAsyncFilterableExecutable<T> : IFilterable<T, IAsyncFilterableExecutable<T>> where T : notnull
 {
+    /// <summary>
+    /// Asynchronously returns the count of the filterable
+    /// </summary>
+    /// <returns></returns>
     public Task<int> Count();
-    public Task<Option<T>> First<TSelected>(Expression<Func<T, TSelected>> selector);
-    public Task<Option<T>> Last<TSelected>(Expression<Func<T, TSelected>> selector);
-    public Task<Result<T>> Single<TSelected>(Expression<Func<T, TSelected>> selector);
+    /// <summary>
+    /// Asynchronously returns an option of the first item of the filterable
+    /// </summary>
+    /// <param name="predicate">The predicate</param>
+    /// <returns></returns>
+    public Task<Option<T>> First(Expression<Func<T, bool>> predicate);
+    /// <summary>
+    /// Asynchronously returns an option of the last item of the filterable
+    /// </summary>
+    /// <param name="predicate">The predicate</param>
+    /// <returns></returns>
+    public Task<Option<T>> Last(Expression<Func<T, bool>> predicate);
+    /// <summary>
+    /// Asynchronously returns an option of the selected type if it is the only item in the filterable.
+    /// </summary>
+    /// <param name="predicate">The predicate</param>
+    /// <returns></returns>
+    public Task<Result<T>> Single(Expression<Func<T, bool>> predicate);
+    /// <summary>
+    /// Gets the AsyncEnumerable of the filterable
+    /// </summary>
+    /// <returns></returns>
     public IAsyncEnumerable<T> ExecuteToAsync();
+    /// <summary>
+    /// Asynchronously gets an IEnumerable of the filterable.
+    /// </summary>
+    /// <returns></returns>
     public Task<IEnumerable<T>> Execute();
 }
